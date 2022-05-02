6365d3b555a622cbda90a5c37c43e235f9e90030
dreamblyss <iwekuba.henry@gmail.com>
2022-05-02 16:07:48 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry Point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+int i;
+
+for (i = 0; i < argc; i++)
+{
+}
+
+printf("%d\n", i - 1);
+
+return (0);
+
+}
