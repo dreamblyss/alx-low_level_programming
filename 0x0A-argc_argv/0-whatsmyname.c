afa9eea307cb9cd0327cb1ed0b7fd7dcdfeebe0c
dreamblyss <iwekuba.henry@gmail.com>
2022-05-02 15:59:48 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,14 +5,13 @@
  * main - prints its name + \n
  * @argc: number of arguments typed
  * @argv: array pointing to arguments
- * Return: array pointing to arguments
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
 
 argc = 0;
- 
+
 printf("%s\n", argv[argc]);
 
 return (0);
