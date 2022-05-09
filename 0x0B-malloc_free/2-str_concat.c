8ad8637a8178bcbd3374f87c6a2679a139e006c8
dreamblyss <iwekuba.henry@gmail.com>
2022-05-09 14:12:04 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 char *s3;
-unsigned int i = 0, j = 0, len1 = 0, len2 = 0;  
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 while (s1 && s1[len1])
 {
@@ -28,11 +28,11 @@ s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 if (s3 == NULL)
 {
 return (NULL);
-}   
+}
 
 i = 0;
-j = 0; 
- 
+j = 0;
+
 if (s1)
 {
 
@@ -44,7 +44,6 @@ i++;
 
 }
 
-
 if (s2)
 {
 
@@ -54,8 +53,7 @@ s3[i] = s2[j];
 i++;
 j++;
 }
-} 
+}
 s3[i] = '\0';
- 
-return (s3); 
+return (s3);
 }
